50e0eb26e4abbc370897526805acfa5e3d7041c8
Tomás Catellani <tomi@iMac-de-Tomas.domain.name>
2020-11-05 08:31:17 -0300

    Archivos innecesarios. Funcionaban como index para mostrar iframes

--- src/app/app-script.js
+++
@@ -1,4 +0,0 @@
-//Todo esto vendría a ser respecto a la pantalla de menú (cdo se presiona un botón, se activa la pantalla de él y se desactivan el resto)
-document.getElementById("pantalla-login").style.display = "none";
-document.getElementById("pantalla-inicio").style.display = "none";
-document.getElementById("pantalla-lotes").style.display = "inline";
