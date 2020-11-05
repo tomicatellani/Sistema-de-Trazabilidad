fe451637a32412a77d33e97aaaecc64111951a92
Tomás Catellani <tomi@iMac-de-Tomas.domain.name>
2020-11-05 08:37:22 -0300

    Transición a menu.html y cambios en la animación

--- src/welcome/welcome-script.js
+++ src/welcome/welcome-script.js
@@ -1,2 +1,15 @@
 document.getElementById('welcome-h1').innerHTML = "Bienvenido,<span id='welcome-bold'></span>";
-document.getElementById('welcome-bold').innerHTML = "asdf" //Here will be name returned by the database according to the username on login.js
+document.getElementById('welcome-bold').innerHTML = " Admin" //Here will be name returned by the database according to the username on login.js
+
+document.getElementById("welcome-h1").addEventListener("animationend", () => {
+  document.getElementById("message-container").style.animationPlayState = "running";
+})
+
+//Cuando la animación termina...
+setTimeout(() => {
+  var a = document.createElement('a'); //Crea una etiqueta <a>
+  a.setAttribute("href", "../menu/menu.html"); //Asigna la ruta
+  document.body.appendChild(a); //La incluye en el body
+  a.click();
+}, 5000);
+
