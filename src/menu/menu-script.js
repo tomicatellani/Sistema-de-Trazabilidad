7720f51f9970fdd0051cbb85f8cd7a0eab867a62
Tomás Catellani <tomi@iMac-de-Tomas.domain.name>
2020-11-05 07:06:19 -0300

    Botón Salir añadido

--- src/menu/menu-script.js
+++ src/menu/menu-script.js
@@ -1,4 +1,8 @@
-document.getElementById("botonera").onload = function(){
-    tamañoBotonera();
-    changeName("Menú principal");
-}
+document.getElementById("menu-btn-salir").onclick = function() { 
+    if( confirm("¿Desea abandonar la aplicación?") == true){  //Probar con sweetalert
+        alert("Salir");
+    } else {
+        alert("Se mantiene en la app");
+    }
+ }
+
