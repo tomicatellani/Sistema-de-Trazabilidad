cf237dbeec85f68afc2fe55cb81f99626e31d71b
Tomás Catellani <tomi@iMac-de-Tomas.domain.name>
2020-11-05 07:02:41 -0300

    Botón atrás añadido. Validación de números en Hectáreas y N.º de Lote. Cultivos cambiado <select>

--- src/lotes/cargar/lotes-cargar-script.js
+++ src/lotes/cargar/lotes-cargar-script.js
@@ -34,3 +34,16 @@ function validarVacios(input){
         return true;
     } 
 }
+
+//Previene que se ingresen símbolos
+document.getElementById("lotes-cargar-numero-lote").addEventListener("keypress", (e) => {
+    if(e.key != "0" && e.key != "1" && e.key != "2" && e.key != "3" && e.key != "4" && e.key != "5" && e.key != "6" && e.key != "7" && e.key != "8" && e.key != "9"){
+        e.preventDefault()
+    }
+})
+
+document.getElementById("lotes-cargar-hectareas").addEventListener("keypress", (e) => {
+    if(e.key != "0" && e.key != "1" && e.key != "2" && e.key != "3" && e.key != "4" && e.key != "5" && e.key != "6" && e.key != "7" && e.key != "8" && e.key != "9"){
+        e.preventDefault()
+    }
+})
